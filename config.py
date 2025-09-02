8bbab21021a6c8fb07aef28f2beb923fe58c48f2
pagal4209 <jaatrudra834@gmail.com>
2025-09-02 10:50:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,8 +24,8 @@ except:
 
 # Pyrogram setup
 class PyroConf(object):
-    API_ID = int(getenv("API_ID", "6"))
-    API_HASH = getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-    BOT_TOKEN = getenv("BOT_TOKEN")
-    SESSION_STRING = getenv("SESSION_STRING")
+    API_ID = int(getenv("API_ID", "21840825"))
+    API_HASH = getenv("API_HASH", "83398eab5af8c8392bc3034f2974299e")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7944120107:AAE71kJhUiXr4PwNIfdOVI_XZwFF5kYXnUs")
+    SESSION_STRING = getenv("SESSION_STRING", "BQFNQ7kAmWo2l0lNwdPzfzDtuq2VcCRXin3jDIQKSeGIxVgehJU4fmbsQibi6E_llqREasr7Q_CoxsQLlug2d2RoIDVU5yaNoYG_lgPJV-F3PK-RV8Q6hzEKZvvvEdLD_rKu5GYDmIVikusJk4hCP1AXjk_qWuH4hG4sIGVql_CfMDOrykIjy1I87zpAtIDDDnhkCXw4HWPy4zw1Qp5QvoLupzo-CNu4UxFbX4Qu4_nD08LufMt3iVk73PxUWGEW3WFgwsu0NmtfjR0EKZl-3XhlXl0cWCKTL9EHmEfjE1P2SbG1r8fWBk2oOaJZrFfupgC0amFnTUUvtPDerzQMxcj8ol6RdAAAAAGtMapdAA")
     BOT_START_TIME = time()
